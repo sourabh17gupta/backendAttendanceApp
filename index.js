275f1b09df7c1c7fea87b9efe712a80620394407
sourabh17gupta <mkktlguptas@gmail.com>
2025-07-12 09:02:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use(cors({
     //this used because my backend and front end are on different port
-    origin: "https://attendanceapp-1.vercel.app", // Allow requests from frontend
+    origin: "https://attendanceapp-three.vercel.app", // Allow requests from frontend
     credentials: true, //Allow cookies & authentication headers
 }));
 
